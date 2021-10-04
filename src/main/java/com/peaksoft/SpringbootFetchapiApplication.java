391bff523e8adfccbea1fd14f3526e41ebe13fbc
aisezimorozbaeva <86042632+aisezimorozbaeva@users.noreply.github.com>
2021-10-04 13:39:47 +0300

    Initial commit

---
+++ src/main/java/com/peaksoft/SpringbootFetchapiApplication.java
@@ -0,0 +1,13 @@
+package com.peaksoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFetchapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFetchapiApplication.class, args);
+    }
+
+}
